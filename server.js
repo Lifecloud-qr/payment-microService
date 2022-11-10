bd89bdccc39fa7d379713d083b33a24107af05f5
yonatanbenezra <yonatanbenezra1@gmail.com>
2022-11-10 17:32:01 +0200

    changed to post

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
 
-app.get("/paymentrequest", (req, res) => {
-  app.get("https://qa-api.lifecloud-qr.com/api/paymentrequest", req.body);
+app.post("/paymentrequest", (req, res) => {
+  axios.post("https://qa-api.lifecloud-qr.com/api/paymentrequest", req.body);
   res.send("Hello World!");
 });
 
